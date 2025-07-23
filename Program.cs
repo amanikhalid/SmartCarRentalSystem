7371e1295adf6a45eb10ba1b83bbf0ee368bfdb1
Amani Khalid <amanikhalidd@gmail.com>
2025-07-23 11:14:01 +0400

    Display available vehicles

--- Program.cs
+++ Program.cs
@@ -6,9 +6,20 @@ namespace SmartCarRentalSystem
 {
     internal class Program
     {
+
+
         static void Main(string[] args)
         {
-           
+
+            Console.WriteLine(" SmartCar Rentals System ");
+            while (true)
+            {
+                Console.WriteLine("\nAvailable Vehicles:"); // Display available vehicles
+                for (int i = 0; i < vehicles.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {vehicles[i].GetInfo()}");
+                }
+            }
         }
     }
 }
